afcc450eeffcf8fed74facd978f7a8662256c900
Sarkies <Ahmed.Sarkes@gmail.com>
2024-03-27 20:40:23 +0200

    update Http

--- src/Http/index.js
+++ src/Http/index.js
@@ -1,10 +1,13 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const Http = axios.create({
   baseURL: process.env.REACT_APP_DASHBOARD_API_URL,
+  withCredentials: true,
   headers: {
     "Content-Type": "multipart/form-data",
     Accept: "application/json",
+    Authorization: `Bearer ${Cookies.get("_auth")}`,
   },
 });
 
